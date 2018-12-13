3fd35dbf98e9b0d8e0536f137d4ac3d68c14e00e
lawrend <douglas_lawrence@mac.com>
2018-12-12 17:48:38 -0800

    verifying repo

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>...if you dare...</p>
           <a
             className="App-link"
             href="https://reactjs.org"
