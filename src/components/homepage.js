4f8312008a83cdb38fad1a2c3f392f1463d3e5df
lawrend <douglas_lawrence@mac.com>
2018-12-16 17:36:34 -0800

    add homepage component and update routes

---
+++ src/components/homepage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => (
+  <div>We are going to do great things in this app</div>
+)
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, withRouter, Switch } from 'react-router-dom';
+import Homepage from './components/homepage.js'
 import ZipCodeSearchBar from './containers/ZipCodeSearchBar';
 import Example from './components/example';
 
@@ -7,11 +8,12 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
+        <Route exact path="/" component={Homepage} />
         <Route exact path="/searchbar" component={ZipCodeSearchBar} />
         <Route component={Example} />
       </Switch>
-    )
-  }
+      )
+}
 };
 
 export default withRouter(Routes);
