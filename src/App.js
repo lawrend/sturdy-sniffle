312a50affba75bb9e1b89c78849fdd37f411422b
lawrend <douglas_lawrence@mac.com>
2019-01-23 17:45:30 -0800

    added Link to homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // the current landing page which renders the Routes component
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Routes from './routes';
+import { Link } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +21,8 @@ class App extends Component {
             Check out Spotify's API
           </a>
           <Routes />
+          <p>
+            <Link to="/songsearch">Search for Songs</Link></p>
         </header>
       </div>
     );
