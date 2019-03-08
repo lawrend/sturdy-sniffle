00363f1a73fc600f0a215fa28d63d6825258d167
lawrend <douglas_lawrence@mac.com>
2019-03-07 16:22:13 -0800

    remove unnecessary files

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import { Item } from 'semantic-ui-react';
 import YouTube from 'react-youtube';
 
 class VideoItem extends Component {
@@ -7,19 +6,18 @@ class VideoItem extends Component {
 
     console.log("video Id: ", this.props.video.id.videoId)
     const source = this.props.video.id.videoId;
-    const opts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        autoplay: 0,
-      },
-    }
+    // const opts = {
+    //   height: '390',
+    //   width: '640',
+    //   playerVars: {
+    //     autoplay: 0,
+    //   },
+    // }
     return(
       <div className="youtube-wrapper">
         <div className="youtube">
           <YouTube
             videoId={source}
-            opts={opts}
             onReady={this._onReady}
           />
             </div>
@@ -32,13 +30,4 @@ class VideoItem extends Component {
 }
 
 export default VideoItem;
-// <Item >
-//   <Item.Image size='medium' src={video.snippet.thumbnails.medium.url} />
-//   <Item.Content>
-//     <Item.Header as='a'>{video.snippet.title}</Item.Header>
-//     <Item.Meta>Meta</Item.Meta>
-//     <Item.Description>{video.snippet.description}</Item.Description>
-//     <Item.Extra>Additional content</Item.Extra>
-//   </Item.Content>
-// </Item>
 
